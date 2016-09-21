561ce1951cf3bc61cc978962a978e657c821048c
vivian <vliao@esri.com>
2016-09-21 12:58:12 -0700

    looks good to me

---
+++ hello-world/README.md
@@ -0,0 +1,3 @@
+# hello-world
+Hi I'm vivian, learning how to use git and jenkins together
+here's a change
---
+++ hello-world/addFiles.bat
@@ -0,0 +1,1 @@
+ "C:\Program Files (x86)\SDL\SDL Passolo\SDL Passolo 2015\pslcmd.exe" C:\Users\vivi8689\Desktop\lpu\LPU_encoding\RHServer_Core.lpu /addsource "sourcefilelist:C:\Users\vivi8689\Desktop\lpu\fileList.txt" sourcelang:"enu" "targetrule:JavaScript_Rule" /update
